296e7726f3382fac42bf813ac11312a608658477
Manoj <manojwaghmare400@gmail.com>
2023-10-08 01:43:36 -0500

    creat card

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,28 @@
 const cl = console.log;
 
+const backDrop = document.getElementById("backDrop");
+const ourModal = document.getElementById("ourModal");
+const addMovie = document.getElementById("addMovie");
+const cancelCard = [...document.querySelectorAll(".cancelCard")];
+
+const onAddBtn = () => {
+    backDrop.classList.toggle("active")
+    ourModal.classList.toggle("active")
+
+    cancelCard.forEach(ele => {
+        // cl(ele)
+        ele.addEventListener("click",onAddBtn)
+    })
+}
+
+
+
+
+
+addMovie.addEventListener("click", onAddBtn);
+
+
+
+
 
 
